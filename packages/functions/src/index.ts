7ee3d97193d4d6a1a87b4142c52b118fb86d8012
Ahmed Eid <a.eid@yandex.com>
2023-11-01 17:23:54 +0200

    ...

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,20 +1,21 @@
 import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma = globalForPrisma.prisma || new PrismaClient({ datasourceUrl: process.env.DATABASE_URL })
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
 export async function main() {
-  const data = await prisma.todo.create({
-    data: {
-      id: Date.now(),
-      name: new Date().toISOString(),
-    },
-  })
-
-  return {
-    ...data,
+  try {
+    const data = await prisma.todo.create({
+      data: { id: Date.now(), name: new Date().toISOString() },
+    })
+    return { ...data }
+  } catch (error) {
+    return "error"
   }
 }
