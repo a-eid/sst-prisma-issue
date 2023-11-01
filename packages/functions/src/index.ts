53e61ccd735218e648d8b75e7140b2eaabce812b
Ahmed Eid <a.eid@yandex.com>
2023-11-02 00:22:26 +0200

    working example finally....

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -2,19 +2,14 @@ import { PrismaClient } from "@prisma/client"
 
 export async function main() {
   try {
-    const globalForPrisma = global as unknown as { prisma: PrismaClient }
-    const prisma = globalForPrisma.prisma || new PrismaClient({ datasourceUrl: process.env.DATABASE_URL })
-    if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
-    const data = await prisma.todo.create({
-      data: {
-        name: new Date().toISOString(),
-      },
-    })
+    const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL })
+    const data = await prisma.todo.create({ data: { name: new Date().toISOString() } })
     return data
   } catch (error: any) {
+    console.log(error)
     return {
       error: error,
-      message: error.message
+      message: error.message,
     }
   }
 }
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,13 +1,44 @@
+import fs from "fs-extra"
+import path from "node:path"
+import * as lambda from "aws-cdk-lib/aws-lambda"
 import { StackContext, Function } from "sst/constructs"
+export const LAYER_MODULES = ["encoding", "@prisma/client/runtime"]
+
+function preparePrismaLayerFiles() {
+  const layerPath = "./layers/prisma"
+  fs.rmSync(layerPath, { force: true, recursive: true })
+  fs.mkdirSync(layerPath, { recursive: true })
+  const files = ["node_modules/.prisma", "node_modules/@prisma/client", "node_modules/prisma/build"]
+  for (const file of files) {
+    // Do not include binary files that aren't for AWS to save space
+    fs.copySync(file, path.join(layerPath, "nodejs", file), {
+      filter: src => !src.endsWith("so.node") || src.includes("rhel"),
+    })
+  }
+}
 
 export function API({ stack }: StackContext) {
-  const lambda = new Function(stack, "trpcLambda", {
+  preparePrismaLayerFiles()
+  const prismaLayer = new lambda.LayerVersion(stack, "PrismaLayer", {
+    description: "Prisma layer",
+    code: lambda.Code.fromAsset("./layers/prisma"),
+  })
+
+  const lambdaFn = new Function(stack, "trpcLambda", {
     handler: "packages/functions/src/index.main",
+    runtime: "nodejs18.x",
+    layers: [prismaLayer],
     timeout: 15,
     memorySize: 1024,
     url: true,
     environment: { DATABASE_URL: process.env.DATABASE_URL! },
-    nodejs: { esbuild: { external: ["@prisma/client", ".prisma"] } },
+    nodejs: {
+      // format: "esm",
+      esbuild: {
+        external: LAYER_MODULES.concat(["@prisma/engines", "@prisma/engines-version", "@prisma/internals"]),
+        sourcemap: true,
+      },
+    },
     copyFiles: [
       {
         from: "packages/functions/prisma/schema.prisma",
@@ -17,10 +48,14 @@ export function API({ stack }: StackContext) {
         from: "packages/functions/node_modules/prisma/libquery_engine-linux-arm64-openssl-1.0.x.so.node",
         to: "packages/functions/src/libquery_engine-linux-arm64-openssl-1.0.x.so.node",
       },
+      {
+        from: "packages/functions/node_modules/prisma/libquery_engine-rhel-openssl-1.0.x.so.node",
+        to: "packages/functions/src/libquery_engine-rhel-openssl-1.0.x.so.node",
+      },
     ],
   })
 
   stack.addOutputs({
-    lambdaUrl: lambda.url,
+    lambdaUrl: lambdaFn.url,
   })
 }
