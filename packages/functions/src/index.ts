8b063f7bc5a90c1093a7d4f5ab17b0a3fc4dde54
Ahmed Eid <a.eid@yandex.com>
2023-11-01 16:33:35 +0200

    ...

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -2,16 +2,7 @@ import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    // datasourceUrl: process.env.DATABASE_URL,
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  })
+export const prisma = globalForPrisma.prisma || new PrismaClient({ datasourceUrl: process.env.DATABASE_URL })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
